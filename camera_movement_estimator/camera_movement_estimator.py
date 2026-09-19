19bc0715808f9577921f88d6ba5bf88318a55bd8
Sathvick Sudarsan <96425065+sathvick-sudarsan@users.noreply.github.com>
2026-09-19 09:55:03 -0700

    Import reconstructed camera movement module

--- camera_movement_estimator/camera_movement_estimator.py
+++ camera_movement_estimator/camera_movement_estimator.py
@@ -2,8 +2,6 @@ import pickle
 import cv2
 import numpy as np
 import os
-import sys 
-sys.path.append('../')
 from utils import measure_distance,measure_xy_distance
 
 class CameraMovementEstimator():
